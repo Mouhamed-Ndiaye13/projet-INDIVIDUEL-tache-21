332346c730eb2e7a717b2ab7e73209facebaad5d
Mouhamed Ndiaye <mouhandiayeuh13@gmail.com>
2026-02-02 21:27:03 -1200

    tay mou nekh 7

--- src/services/api.js
+++ src/services/api.js
@@ -21,6 +21,26 @@
 
 // export default api;
 
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "https://projet-individuel-tache-21.onrender.com/api",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
+
+// api.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default api;
+
+
 import axios from "axios";
 
 const api = axios.create({
@@ -32,9 +52,24 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) {
+
+  //  routes publiques → PAS de token
+  const publicEndpoints = [
+    "/auth/users/",
+    "/auth/jwt/create/",
+    "/auth/users/reset_password/",
+    "/auth/users/reset_password_confirm/",
+    "/auth/users/activation/",
+  ];
+
+  const isPublic = publicEndpoints.some((url) =>
+    config.url?.startsWith(url)
+  );
+
+  if (token && !isPublic) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
 });
 
