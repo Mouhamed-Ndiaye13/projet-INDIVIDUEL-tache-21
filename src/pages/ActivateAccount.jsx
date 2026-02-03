cd6c5fed1f8f84c9c1dfc97d89f7f296f208fc98
Mouhamed Ndiaye <mouhandiayeuh13@gmail.com>
2026-02-02 17:16:53 -1200

    tay mou nekh 2

--- src/pages/ActivateAccount.jsx
+++ src/pages/ActivateAccount.jsx
@@ -1,18 +1,22 @@
 import { useEffect, useState } from "react";
-import { useParams, Link, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import api from "../services/api";
 
 export default function ActivateAccount() {
   const { uid, token } = useParams();
-  const navigate = useNavigate();
   const [status, setStatus] = useState("pending"); // pending, success, error
+  const navigate = useNavigate();
 
   useEffect(() => {
     const activate = async () => {
       try {
-        await api.post("/auth/users/activation/", { uid, token });
+        await api.post(`/users/activate/${uid}/${token}/`);
         setStatus("success");
-        setTimeout(() => navigate("/"), 2000);
+
+        // Redirection vers la page login après 2 secondes
+        setTimeout(() => {
+          navigate("/", { replace: true });
+        }, 2000);
       } catch {
         setStatus("error");
       }
@@ -25,10 +29,9 @@ export default function ActivateAccount() {
       <div className="w-full max-w-md bg-white/10 backdrop-blur-xl p-8 rounded-2xl text-center text-white">
         {status === "pending" && <p>Activation en cours...</p>}
         {status === "success" && (
-          <>
-            <p className="text-green-400 mb-4">Compte activé avec succès !</p>
-            <p>Redirection vers la connexion...</p>
-          </>
+          <p className="text-green-400 mb-4">
+            Compte activé avec succès ! Redirection vers la connexion...
+          </p>
         )}
         {status === "error" && <p className="text-red-400">Lien d’activation invalide ou expiré.</p>}
       </div>
